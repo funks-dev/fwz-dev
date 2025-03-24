b75a05224cab2552daae1a45a3634ca38a4538d6
Fawwaz Mufid W <mahesfawwaz79@gmail.com>
2025-03-24 14:48:09 +0700

    Update console.js

--- console.js
+++ console.js
@@ -1,3 +1,11 @@
 // Hey this is comment
 console.log("Hello World")
 console.log("Hello part 2") // this is 2nd time console.log
+
+function main() {
+  return null;
+}
+main()
+
+// console.log(main)
+// console.log(main) part 2
